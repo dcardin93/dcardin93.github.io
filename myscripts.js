1ff41e3426a50298acfc78c0c111d21b4ed210ea
dcardin93 <dcardin93@gmail.com>
2017-09-26 10:04:30 +0200

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,6 +2,7 @@
 function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
+  LoadDays(this);
   return false;
 }
 
